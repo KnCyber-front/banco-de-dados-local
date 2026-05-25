81e256a89aeeb76636c3d42046ad6dc19aeb9bb5
famil <familia5215.com@gmail.com>
2026-05-25 01:31:13 -0300

    tela de login com efeitos visuais

---
+++ teste.py
@@ -0,0 +1,14 @@
+site = {
+    "home": "anuncios", 
+    "home": "sair",
+    "anuncios": "entrar compra",
+    "anuncios": "sair compra"
+
+ }
+
+
+print("vc esta em nosso site")
+print({"site": site})
+pagina = input("digite que deseja acessar: ")
+if pagina == "home":
+    print("vc esta na pagina home")
